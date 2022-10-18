3801f198da58d1f438e8dd0cb2d15fb74392c2e4
Kiefer Levine <kiefer_8@hotmail.com>
2022-10-18 14:50:51 -0400

    Connected app to postgres database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PW,
+      database: process.env.DATABASE,
+      entities: []
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
