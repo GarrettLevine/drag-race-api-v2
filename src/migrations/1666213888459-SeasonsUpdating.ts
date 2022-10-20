f6a36fbc6e0562ca389f89cefaba6b402e5ce3aa
Kiefer Levine <kiefer_8@hotmail.com>
2022-10-20 15:55:59 -0400

    Updated migration to fully migrate Seasons table, utilizing queryRunner methods

--- src/migrations/1666213888459-SeasonsUpdating.ts
+++ src/migrations/1666213888459-SeasonsUpdating.ts
@@ -1,17 +1,46 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class SeasonsUpdating1666213888459 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(
-            `Alter TABLE "seasons" ADD COLUMN "year" INTEGER NOT NULL`
+        await queryRunner.createTable(
+            new Table({
+                name: 'seasons',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'int',
+                        isGenerated: true,
+                        isPrimary: true
+                    },
+                    {
+                        name: 'seasonNumber',
+                        type: 'varchar(20)',
+                        isNullable: false
+                    },
+                    {
+                        name: 'winnerId',
+                        type: 'int',
+                        isNullable: true
+                    },
+                    {
+                        name: 'year',
+                        type: 'int',
+                        isNullable: false
+                    },
+                    {
+                        name: 'image_url',
+                        type: 'varchar',
+                        isNullable: false
+                    }
+                ]
+            }),
+             true
         );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(
-            `ALTER TABLE "seasons" DROP COLUMN "year"`
-        );
+        await queryRunner.dropTable('seasons');
     }
 
 }
